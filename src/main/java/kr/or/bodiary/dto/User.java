d48255cc559069a4950b042df2287369f4bca9ce
zzanggood <61582619+zzanggood@users.noreply.github.com>
2020-07-01 16:37:33 +0900

    탬플릿 1.0.0

---
+++ src/main/java/kr/or/bodiary/dto/User.java
@@ -0,0 +1,21 @@
+package kr.or.bodiary.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@ToString
+@Getter
+@Setter
+
+public class User {
+	private String id;
+	private String pwd;
+	/*
+	 * public String getId() { return id; } public void setId(String id) { this.id =
+	 * id; } public String getPwd() { return pwd; } public void setPwd(String pwd) {
+	 * this.pwd = pwd; }
+	 */
+	
+}
