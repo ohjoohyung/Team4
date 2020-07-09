db5155061985f94569f579c61d715565acc88c25
liveforownhappiness1 <liveforownhappiness@gmail.com>
2020-07-09 16:57:34 +0900

    search/data값 보내기빼고 끝

--- src/main/java/kr/or/bodiary/exercise/dao/exerciseDAO.java
+++ src/main/java/kr/or/bodiary/exercise/dao/exerciseDAO.java
@@ -2,10 +2,17 @@ package kr.or.bodiary.exercise.dao;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import kr.or.bodiary.exercise.dto.exerciseDTO;
 
 
 public interface exerciseDAO {
 	public List<exerciseDTO> getExerciseListByName(String excs_name) throws ClassNotFoundException, SQLException;
+	public List<exerciseDTO> getExercisecListByName() throws ClassNotFoundException, SQLException;
+	public List<exerciseDTO> getExercisenListByName() throws ClassNotFoundException, SQLException;
+	public List<exerciseDTO> exercisepartSearch(String excs_body_part) throws ClassNotFoundException, SQLException;
+	public List<exerciseDTO> exerciseEquiSearch(String excs_body_part) throws ClassNotFoundException, SQLException;
+	public List<exerciseDTO> exercisepartequiSearch(Map<String, String> exerciseparteuqi);
+	public List<exerciseDTO> exerciseadd(int excs_seq) throws ClassNotFoundException, SQLException;
 }
