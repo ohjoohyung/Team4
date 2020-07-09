7d88640716026498406c78b6b783f19917825bcc
liveforownhappiness1 <liveforownhappiness@gmail.com>
2020-07-09 16:51:19 +0900

    seach 데이터값남기는것 빼고 완성

--- src/main/java/kr/or/bodiary/exercise/dto/exerciseDTO.java
+++ src/main/java/kr/or/bodiary/exercise/dto/exerciseDTO.java
@@ -1,24 +1,22 @@
 package kr.or.bodiary.exercise.dto;
 
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+
+import lombok.Data;
 
 
-@ToString
-@Getter
-@Setter
 
+@Data
 public class exerciseDTO {
 	private int excs_seq ;
 	private String excs_kind ;
 	private String excs_name ;
-	private int excs_cal_per_minweight ;
+	private float excs_cal_per_minweight;
 	private String excs_body_part ;
 	private String excs_equipment ;
 	private String excs_description ;
 	private String excs_video_link ;
-	private int excs_level ;
+	private float excs_level ;
+	
 	
 }
