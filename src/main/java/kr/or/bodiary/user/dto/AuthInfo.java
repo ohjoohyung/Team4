95c873f9674e07578eb7e2625370f6cc5e9a2a9c
Eomjihee <eomjihee.code@gmail.com>
2020-07-24 18:31:26 +0900

    콜백 경유 페이지 마무리

---
+++ src/main/java/kr/or/bodiary/user/dto/AuthInfo.java
@@ -0,0 +1,20 @@
+package kr.or.bodiary.user.dto;
+
+public class AuthInfo {
+	   private String clientId;
+	   private String clientSecret;
+
+
+	   public AuthInfo(String clientId, String clientSecret) {
+	      this.clientId = clientId;
+	      this.clientSecret = clientSecret;
+	   }
+	   public String getClientId() {
+	      return clientId;
+	   }
+	   public String getClientSecret() {
+	      return clientSecret;
+	   }
+	   
+	   
+	}
