1b3e8153efeff728c734278cbfff45d57eaba159
zzanggood <61582619+zzanggood@users.noreply.github.com>
2020-07-28 15:36:15 +0900

    최종 수정

--- src/main/java/kr/or/bodiary/main/controller/MainController.java
+++ src/main/java/kr/or/bodiary/main/controller/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.or.bodiary.freeBrd.dto.FreeBrdDTO;
 import kr.or.bodiary.main.service.MainService;
@@ -25,7 +26,7 @@ public class MainController {
 	public void setMainService(MainService mainservice) {
 		this.mainservice = mainservice;
 	}
-	@RequestMapping("/")
+	@RequestMapping(value="/", method =RequestMethod.GET)
 	public String index() {
 		return "index";
 	}
